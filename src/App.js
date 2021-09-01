a47df5f98b36eacae52e183f37fad7fd763224b7
Jonnie Spratley <jspratle@cisco.com>
2021-09-01 13:01:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {WebexMeetingWidget} from '@webex/widgets';
+
+import '@webex/widgets/dist/webexWidgets.css';
+
 import './App.css';
+
+
+const meetingDestination = '';
+const accessToken = '';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Space Widget</h2>
+     <WebexMeetingWidget 
+     accessToken={accessToken}
+     meetingDestination={meetingDestination} />
     </div>
   );
 }
